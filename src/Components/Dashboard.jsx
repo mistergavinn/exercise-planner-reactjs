7c4341ba03818078f124abeaa0509be29ba55b80
Gavin Er <gaviner95@gmail.com>
2022-06-27 17:23:25 +0800

    Lifted state from WeekView to Dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -3,7 +3,36 @@ import StatDashboard from './Displays/StatDashboard';
 import WeekView from './Displays/WeekView';
 
 class Dashboard extends Component {
-    state = {  } 
+  state = { 
+    dayViews: [
+        { id: 1, day: 'Monday', adddate: 0, distance: 0, duration:0, notes:''}, 
+        { id: 2, day: 'Tuesday', adddate: 1, distance: 0, duration:0, notes:''},  
+        { id: 3, day: 'Wednesday', adddate: 2, distance: 0, duration:0, notes:''},
+        { id: 4, day: 'Thursday', adddate: 3, distance: 0, duration:0, notes:''},
+        { id: 5, day: 'Friday', adddate: 4, distance: 0, duration:0, notes:''},
+        { id: 6, day: 'Saturday', adddate: 5, distance: 0, duration:0, notes:''},
+        { id: 7, day: 'Sunday', adddate: 6, distance: 0, duration:0, notes:''}  
+    ]
+ }
+ 
+ handleDistChange = (dayId, distance) => {
+  const dayViews = [...this.state.dayViews];
+  const index = dayViews.map(c => c.id).indexOf(dayId);
+  dayViews[index].distance = distance;
+  this.setState({dayViews});
+}
+handleDurationChange = (dayId, duration) => {
+  const dayViews = [...this.state.dayViews];
+  const index = dayViews.map(c => c.id).indexOf(dayId);
+  dayViews[index].duration = duration;
+  this.setState({dayViews});
+}
+handleNotesChange = (dayId, notes) => {
+  const dayViews = [...this.state.dayViews];
+  const index = dayViews.map(c => c.id).indexOf(dayId);
+  dayViews[index].notes = notes;
+  this.setState({dayViews});
+}
     render() { 
         return (
           <div>
@@ -16,7 +45,10 @@ class Dashboard extends Component {
 
             <div className="row border-outline">
               <div className="col-12 week-view-container border-outline">
-                <WeekView />
+                <WeekView dayViews={this.state.dayViews}
+                onDistChange ={this.handleDistChange}
+                onDurationChange ={this.handleDurationChange}
+                onNotesChange ={this.handleNotesChange}/>
               </div>
             </div>
           </div>
--- src/Components/Displays/WeekView.jsx
+++ src/Components/Displays/WeekView.jsx
@@ -3,48 +3,18 @@ import DayView from './DayView';
 import WeeklyStats from './WeeklyStats';
 
 class WeekView extends Component {
-    state = { 
-        dayViews: [
-            { id: 1, day: 'Monday', adddate: 0, distance: 0, duration:0, notes:''}, 
-            { id: 2, day: 'Tuesday', adddate: 1, distance: 0, duration:0, notes:''},  
-            { id: 3, day: 'Wednesday', adddate: 2, distance: 0, duration:0, notes:''},
-            { id: 4, day: 'Thursday', adddate: 3, distance: 0, duration:0, notes:''},
-            { id: 5, day: 'Friday', adddate: 4, distance: 0, duration:0, notes:''},
-            { id: 6, day: 'Saturday', adddate: 5, distance: 0, duration:0, notes:''},
-            { id: 7, day: 'Sunday', adddate: 6, distance: 0, duration:0, notes:''}  
-        ]
-     } 
-
-     handleDistChange = (dayId, distance) => {
-        const dayViews = [...this.state.dayViews];
-        const index = dayViews.map(c => c.id).indexOf(dayId);
-        dayViews[index].distance = distance;
-        this.setState({dayViews});
-     }
-     handleDurationChange = (dayId, duration) => {
-        const dayViews = [...this.state.dayViews];
-        const index = dayViews.map(c => c.id).indexOf(dayId);
-        dayViews[index].duration = duration;
-        this.setState({dayViews});
-     }
-     handleNotesChange = (dayId, notes) => {
-        const dayViews = [...this.state.dayViews];
-        const index = dayViews.map(c => c.id).indexOf(dayId);
-        dayViews[index].notes = notes;
-        this.setState({dayViews});
-     }
 
     render() { 
         return (
             <div className="border-outline ">
                 <div className="col-9">
                     <div className='test-area'>
-                        {this.state.dayViews.map(dayView => 
+                        {this.props.dayViews.map(dayView => 
                         <DayView key={dayView.id} 
                         dayView = {dayView} 
-                        onDistChange ={this.handleDistChange}
-                        onDurationChange ={this.handleDurationChange}
-                        onNotesChange ={this.handleNotesChange}
+                        onDistChange={(dayId, distance) => this.props.onDistChange(dayId, distance)}
+                        onDurationChange={(dayId, duration) => this.props.onDurationChange(dayId, duration)}
+                        onNotesChange={(dayId, notes) => this.props.onNotesChange(dayId, notes)}
                         />)}
                     </div>
                 </div>
